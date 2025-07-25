d5bc5b9439435bcc53e297a4535d06cd323d35c0
wazzja <wasja@velour.de>
2025-07-25 14:00:29 +0200

    some more flexibilty added, can now change Type to CLASS or TAG

--- files/fetch.py
+++ files/fetch.py
@@ -2,7 +2,12 @@ import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from config import WIDGET
+from config import SELECTOR, SELECTOR_TYPE
+
+bymap = {
+        "TAG": By.TAG_NAME,
+        "CLASS": By.CLASS_NAME,
+}
 
 def fetch_url(url):
     options = uc.ChromeOptions()
@@ -14,9 +19,12 @@ def fetch_url(url):
 
     try:
         driver.get(url)
-        WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.TAG_NAME, WIDGET)))
+        WebDriverWait(driver, 20).until(EC.presence_of_element_located((bymap[SELECTOR_TYPE], SELECTOR)))
         WebDriverWait(driver, 30).until_not(EC.presence_of_element_located((By.CLASS_NAME, "fa-spinner")))
-        target = driver.find_elements(By.TAG_NAME, WIDGET)
+        if SELECTOR_TYPE == "CLASS":
+            target = driver.find_elements(By.CSS_SELECTOR, f"div.content.{SELECTOR}")
+        else:
+            target = driver.find_elements(bymap[SELECTOR_TYPE], SELECTOR)
         return [i.get_attribute("innerHTML") for i in target]
     finally:
         driver.quit()
